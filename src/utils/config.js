14b3fed7dcfc7d4ed36b64a039cf73c12755dd77
Maithil Wade <wmaithil@gmail.com>
2021-08-08 22:44:16 +0530

    firstcommit

---
+++ src/utils/config.js
@@ -0,0 +1,17 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4z4WXqkndvU0Ql8Fh2LAbmk456yxe_iw",
+  authDomain: "mygitapp-f0dff.firebaseapp.com",
+  databaseURL: "https://mygitapp-f0dff-default-rtdb.firebaseio.com",
+  projectId: "mygitapp-f0dff",
+  storageBucket: "mygitapp-f0dff.appspot.com",
+  messagingSenderId: "299653664539",
+  appId: "1:299653664539:web:08df6199f2e68d11f38dbe",
+  measurementId: "G-EQ6EZ62C2S"
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
